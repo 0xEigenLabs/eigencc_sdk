b160b52b4a6f4b99df8baf7d40de0644f81cc35a
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2021-12-19 20:45:49 +0800

    Test dev, WIP

--- test/tee_task.test.ts
+++ test/tee_task.test.ts
@@ -1,9 +1,10 @@
-
 import { expect } from "chai";
-const tee_task = require('bindings')('tee_task')
+require("./src/util");
+
+const tee_task = require("bindings")("tee_task");
 
 require("dotenv").config();
 
 describe("Tee Task", () => {
-    expect(tee_task.init()).to.eq(0);
+  expect(tee_task.init()).to.eq(0);
 });
