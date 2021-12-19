d6e49cfc9b88aeb8ff926c95306881e73a7d32f7
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2021-12-19 21:46:59 +0800

    Add submit_task test code

--- test/tee_task.test.ts
+++ test/tee_task.test.ts
@@ -15,10 +15,6 @@ const require_env_variables = (envVars) => {
 };
 
 require_env_variables([
-  "TEESDK_METHOD",
-  "TEESDK_ARGS",
-  "TEESDK_UID",
-  "TEESDK_TOKEN",
   "TEESDK_AUDITOR_BASE_DIR",
   "TEESDK_AUDITOR_NAME",
   "TEESDK_ENCLAVE_INFO_PATH",
@@ -28,13 +24,13 @@ const AUDITOR_BASE_DIR = process.env["TEESDK_AUDITOR_BASE_DIR"];
 // auditor_name, e.g., "godzilla"
 const AUDITOR_NAME = process.env["TEESDK_AUDITOR_NAME"];
 const ENCLAVE_INFO_PATH = process.env["TEESDK_ENCLAVE_INFO_PATH"];
-const METHOD = process.env["TEESDK_METHOD"];
-const ARGS = process.env["TEESDK_ARGS"];
-const UID = process.env["TEESDK_UID"];
-const TOKEN = process.env["TEESDK_TOKEN"];
 const PUB = `${AUDITOR_BASE_DIR}/${AUDITOR_NAME}/${AUDITOR_NAME}.public.der`;
 const PRI = `${AUDITOR_BASE_DIR}/${AUDITOR_NAME}/${AUDITOR_NAME}.sign.sha256`;
 
 describe("Tee Task", () => {
   expect(TEE_TASK.init(PUB, PRI, ENCLAVE_INFO_PATH, 8082)).to.eq(0);
+  expect(TEE_TASK.submit_task("echo", "Hello", "uid", "token")).to.eq(
+    "Hello, Eigen"
+  );
+  expect(TEE_TASK.release()).to.eq(0);
 });
